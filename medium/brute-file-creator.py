8b8db0f804a908ebcc3af500c4e60cd798654e2d
atrox3d <atrox3d@gmail.com>
2021-07-13 18:03:01 +0200

    fix options

--- medium/brute-file-creator.py
+++ medium/brute-file-creator.py
@@ -6,24 +6,28 @@ Link to Github - https://github.com/r-sajal/Ethical-Hacking
 ####################################################################################################################
 
 # important libraries
-import itertools # efficient looping
-import string    # string functions
-import time      # time checking
+import itertools  # efficient looping
+import string  # string functions
+import sys
+import time  # time checking
+import os
+
+from helpers import options
 
 
-def password_wordlist(start_range=8,end_range=10,file_name="brute.txt"):
+def password_wordlist(start_range=8, end_range=10, file_name="brute.txt"):
     # string with all characters needed or have potential for being password
     chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + '@' + '#' + '$' + '.'
     # attempts counter
     attempts = 0
     # open file
-    f = open(file_name,'a')
+    f = open(file_name, 'a')
 
     for password_length in range(start_range, end_range):
-        for guess in itertools.product(chars,repeat=password_length):
+        for guess in itertools.product(chars, repeat=password_length):
             attempts += 1
             guess = ''.join(guess)
-            f.write(guess) # write in file
+            f.write(guess)  # write in file
             f.write("\n")
             print(guess, attempts)
 
@@ -31,18 +35,33 @@ def password_wordlist(start_range=8,end_range=10,file_name="brute.txt"):
     f.close()
 
 
+def show_help():
+    print(os.path.basename(sys.argv[0]), "-h -s start -e end -f filename")
 
-start_range = 8
-end_range = 10
-file_name = "brute_password_list.txt"
 
+if __name__ == '__main__':
+    opts = options.parse_options("hs:e:f:", helpfn=show_help)
+    start_range = int(opts.s)
+    end_range = int(opts.e)
+    file_name = opts.f
+    if not all((start_range, end_range, file_name)):
+        show_help()
+        exit()
+
+    print(start_range, end_range, file_name)
+
+
+# exit()
+# start_range = 8
+# end_range = 10
+# file_name = "brute_password_list.txt"
 
 start_time = time.time()
 # Main function Call
-password_wordlist(start_range,end_range,file_name)
+password_wordlist(start_range, end_range, file_name)
 
 end_time = time.time()
 
-print(end_time-start_time) # print the total time for operation
+print(end_time - start_time)  # print the total time for operation
 
 ##### Code by Sajal Rastogi
